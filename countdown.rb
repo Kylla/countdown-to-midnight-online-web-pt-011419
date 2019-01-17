f0dfb4905e6fe6af91f3f62016136805ac3e31c9
Kylla Harris <kylla.harris@gmail.com>
2019-01-17 04:34:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,12 @@
-def countdown
+def countdown(number)
   
     while number > 0
     puts "#{number} SECOND(S)!"
   number -= 1
+end
   "HAPPY NEW YEAR!"
 end
 #def countdown_with_sleep
  # sleep 5
 #end
-end
+
